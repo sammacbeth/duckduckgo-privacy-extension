9e7a8cf4574e0f8ad516beab123bf7c11fca197e
Sam Macbeth <smacbeth@duckduckgo.com>
2021-02-12 14:03:12 +0100

    Add test for 1st party cookie expiration

--- integration-test/background/storage.js
+++ integration-test/background/storage.js
@@ -16,8 +16,7 @@ describe(`On https://${testPageDomain}/privacy-protections/storage-blocking/`, (
                 // once we see this url, we can consider the test completed
                 iframeFullyLoaded |= req.url() === `https://${thirdPartyDomain}/privacy-protections/storage-blocking/iframe.js`
             })
-            await page.goto(`https://${testPageDomain}/privacy-protections/storage-blocking/`, { waitUntil: 'networkidle0' })
-            await page.evaluate('storeData()')
+            await page.goto(`https://${testPageDomain}/privacy-protections/storage-blocking/?store`, { waitUntil: 'networkidle0' })
             // eslint-disable-next-line no-unmodified-loop-condition
             while (!iframeFullyLoaded) {
                 await wait.ms(100)
@@ -30,25 +29,32 @@ describe(`On https://${testPageDomain}/privacy-protections/storage-blocking/`, (
         await harness.teardown(browser)
     })
 
-    it('does not block 1st party HTTP cookies', async () => {
+    it('does not block 1st party HTTP cookies', () => {
         const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === testPageDomain)
         expect(headerCookie).toBeTruthy()
         expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
     })
 
-    it('blocks 3rd party HTTP cookies', async () => {
+    it('blocks 3rd party HTTP cookies', () => {
         const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === thirdPartyDomain)
         expect(headerCookie).toBeUndefined()
     })
 
-    it('does not block 1st party JS cookies', async () => {
-        const headerCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === testPageDomain)
-        expect(headerCookie).toBeTruthy()
-        expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
+    it('does not block 1st party JS cookies', () => {
+        const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === testPageDomain)
+        expect(jsCookie).toBeTruthy()
+        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
     })
 
-    it('blocks 3rd party JS cookies', async () => {
-        const headerCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === thirdPartyDomain)
-        expect(headerCookie).toBeUndefined()
+    it('blocks 3rd party JS cookies', () => {
+        const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === thirdPartyDomain)
+        expect(jsCookie).toBeUndefined()
+    })
+
+    it('restricts expiry of 1st party JS cookies set from 3rd party scripts', () => {
+        const jsCookie = cookies.find(({ name, domain }) => name === 'tpdata' && domain === testPageDomain)
+        expect(jsCookie).toBeTruthy()
+        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
+        expect(jsCookie.expires).toBeLessThan((Date.now() / 1000) + 604800) // 7 days in the future
     })
 })
